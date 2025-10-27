dbd918a059009097677639ac43c59840ad314b48
NiyateeD <K1638427@students.katyisd.org>
2025-10-27 09:22:44 -0500

    Create GamePanel.java

---
+++ GamePanel.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.util.*;
+
+public class GamePanel {
+
+	
+	public GamePanel() {
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	
+}
