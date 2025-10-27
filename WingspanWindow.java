b50ecf52c2b0339c13d9597151e25e14f5ec07ab
WangCatherineK1223048 <k1223048@students.katyisd.org>
2025-10-27 09:07:11 -0500

    Add WingspanWindow class for main application window

---
+++ WingspanWindow.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class WingspanWindow extends JFrame {
+	
+	private static final int WIDTH = 1600;
+	private static final int HEIGHT = 1000;
+	
+	public WingspanWindow(String name) {
+		super(name);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		setVisible(true);
+	}
+}
