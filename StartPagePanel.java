cf92ea9ad61936eafbc433b003cd95097b37601b
NiyateeD <K1638427@students.katyisd.org>
2025-10-27 09:14:17 -0500

    Create StartPagePanel.java

---
+++ StartPagePanel.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.util.*;
+
+public class StartPagePanel implements MouseListener{
+
+	BufferedImage logo;
+	
+	public StartPagePanel() {
+		addMouseListener();
+		
+		try {
+			logo = ImageIO.read(StartPagePanel.class.getResource("/images/download.jpg"));
+		} 
+		catch(Exception e) {
+			System.out.println("Error");
+		}
+	}
+	
+
+	public void paint(Graphics g) {
+		g.drawImage(logo,500, 200, logo.getWidth(), logo.getHeight(), null);
+	}
+	private void addMouseListener() {}
+	public void mouseClicked(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	
+	
+}
