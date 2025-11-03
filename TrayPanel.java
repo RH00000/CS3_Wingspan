e8f32aa13081b73266263c7186f588745c9ae63e
NiyateeD <K1638427@students.katyisd.org>
2025-11-03 09:26:15 -0600

    Add TrayPanel class with initial methods

--- TrayPanel.java
+++ TrayPanel.java
@@ -1,4 +1,28 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
 public class TrayPanel {
 
+	public ArrayList<Bird> trayCards = new ArrayList<>();
+	
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void drawTray(Graphics g) {
+		
+	}
+	public void updateTray(ArrayList<Bird> trayCards) {
+		
+	}
 }
