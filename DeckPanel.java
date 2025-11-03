60ae9b3ae6ab05af7e5ae23116e1c745edd33dff
NiyateeD <K1638427@students.katyisd.org>
2025-11-03 09:27:00 -0600

    Add DeckPanel class with initial structure

--- DeckPanel.java
+++ DeckPanel.java
@@ -1,4 +1,31 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
 public class DeckPanel {
 
+	public ArrayList<Bird> birdsDiscarded= new ArrayList<>();
+	public ArrayList<Bonus> bonusDiscarded= new ArrayList<>();
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void drawDeck(Graphics g) {
+		
+	}
+	
+	public void updateDeck(ArrayList<Bird> birdsDiscarded, ArrayList<Bonus> bonusDiscarded) {
+		
+	}
+
+	
 }
