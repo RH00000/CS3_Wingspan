d2947ea007a58664f0aca7f901fcb5535713f56b
NiyateeD <K1638427@students.katyisd.org>
2025-11-03 09:25:57 -0600

    Add RoundGoalPanel class with initial structure

--- RoundGoalPanel.java
+++ RoundGoalPanel.java
@@ -1,4 +1,33 @@
 
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.util.*;
+
+
 public class RoundGoalPanel {
+
+	public ArrayList<RoundGoal> goals = new ArrayList<>();
+	public HashMap<RoundGoal, int[]> roundScores = new ArrayList<>();
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void drawRoundGoal(Graphics g) {
+		
+	}
+	
+	public void updateRoundGoal(Player p, int round, int score) {
+		
+	}
 
 }
