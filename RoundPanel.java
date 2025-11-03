dbf470fb31ab8f0ca20f50c497a32795e1f6323a
NiyateeD <K1638427@students.katyisd.org>
2025-11-03 08:57:54 -0600

    Create RoundPanel.java

---
+++ RoundPanel.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.util.*;
+
+public class RoundPanel {
+	
+	private GameStatePanel gamestatepanel;
+	private BirdFeederPanel birdfeederpanel;
+	private HandPanel handpanel;
+	private RoundGoalPanel roundgoalpanel;
+	private TrayPanel traypanel;
+	private DeckPanel deckpanel;
+	private ActionPanel actionpanel;
+	private BoardPanel boardpanel;
+	
+	public RoundPanel() {
+		
+		try {
+			
+		}
+		catch(Exception e) {
+			System.out.println("error!");
+		}
+	}
+	
+	
+	public void paint(Graphics g) {
+		
+	}
+
+}
