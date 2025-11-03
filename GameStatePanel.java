39bdb9400fc1a23cab6851a374711a27614d0923
NiyateeD <K1638427@students.katyisd.org>
2025-11-03 09:25:10 -0600

    Update GameStatePanel.java

--- GameStatePanel.java
+++ GameStatePanel.java
@@ -1,4 +1,33 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.util.*;
 
 public class GameStatePanel {
-
+	public ArrayList<Integer> numFood = new ArrayList<>();
+	public ArrayList<Integer> numCards = new ArrayList<>();
+	public int numActions;
+	public int numEggs;
+	public boolean isFirst;
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void drawGameState(Graphics g) {
+		
+	}
+	
+	public void updateGameState(ArrayList<Integer> numFood, ArrayList<Integer> foodInside, ArrayList<Integer> numCards, int numActions, int numEggs, boolean isFirst ) {
+		
+		
+	}
 }
