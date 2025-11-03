5a48674628d4a7e92fa969bfb970d4d5f754d556
NiyateeD <K1638427@students.katyisd.org>
2025-11-03 09:25:37 -0600

    Add HandPanel class with initial structure

--- HandPanel.java
+++ HandPanel.java
@@ -1,4 +1,33 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.util.*;
 
 public class HandPanel {
+	public Player player;
+	public ArrayList<Bird> birdCardsHolding = new ArrayList<>();
+	public ArrayList<Bonus> bonusCardsHolding = new ArrayList<>();
+	HashMap<Integer, Integer> food = new HashMap<>();
+	
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void drawHand(Graphics g) {
+		
+	}
+	
+	public void updateHand(Player player, ArrayList<Bird> birdCardHolding, ArrayList<Bonus> bonusCardHolding, HashMap<Integer, Integer> food ) {
+		
+	}
 
 }
