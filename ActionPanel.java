9f84677c0de0a013edaa28df03b79981a67532f5
NiyateeD <K1638427@students.katyisd.org>
2025-11-04 08:46:10 -0600

    Update ActionPanel.java

--- ActionPanel.java
+++ ActionPanel.java
@@ -1,4 +1,44 @@
+import java.util.*;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
 
 public class ActionPanel {
 
+	public JButton gainFoodBtn = new JButton();
+	public JButton layEggsBtn = new JButton();
+	public JButton drawCardsBtn = new JButton();
+	public JButton playBirdBtn = new JButton();
+	public JButton exchangeFood = new JButton();
+	public JButton exchangeDice = new JButton();
+	public JButton exchangeCard = new JButton();
+	public JButton rerollDice = new JButton();
+
+
+	public void paint(Graphics g) {
+		
+	}
+
+	public void drawActions(Graphics g) {
+		
+	}
+	
+	public void updateActionas(GameEvent e) {
+		
+	}
+
+
+
+
+	
+	
 }
