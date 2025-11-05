3533b32d6d8214b69d71a3dd1c0ab640c4f7de0b
WangCatherineK1223048 <k1223048@students.katyisd.org>
2025-11-05 09:28:41 -0600

    Update BoardPanel constructor and paint method
    Set preferred size and background color for testing.

--- BoardPanel.java
+++ BoardPanel.java
@@ -20,6 +20,10 @@ public class BoardPanel extends JPanel {
 	
 	
 	public BoardPanel() {
+		
+		setPreferredSize(new Dimension(800, 500));
+		setBackground(Color.green); //testing only
+		
 		try {
 			b = ImageIO.read(StartPagePanel.class.getResource("/images/board.jpg"));
 		} 
@@ -29,9 +33,8 @@ public class BoardPanel extends JPanel {
 	}
 	
 	public void paint(Graphics g) {
-
-		g.drawImage(b,0 , 0, this.getWidth(), this.getHeight(), null);
+		
+		super.paint(g);
+		g.drawImage(b,10 , 30, b.getWidth(), b.getHeight(), null);
 	}
 }
-
-
