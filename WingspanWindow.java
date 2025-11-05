c4efafcaf2ae86fd5229ea1721cff4e0bc2e7360
WangCatherineK1223048 <k1223048@students.katyisd.org>
2025-11-04 23:53:30 -0600

    Update WingspanWindow.java

--- WingspanWindow.java
+++ WingspanWindow.java
@@ -1,20 +1,10 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
 import javax.swing.*;
 
 public class WingspanWindow extends JFrame {
 
 	private static final int WIDTH = 1600;
 	private static final int HEIGHT = 1000;
-	
-	StartPagePanel startPanel;
-	RoundPanel roundPanel;
-
-	JButton btn;
 
 	public WingspanWindow(String name) {
 
@@ -22,52 +12,9 @@ public class WingspanWindow extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(WIDTH, HEIGHT);
 
-		addStart();
-		buttonClicked();
+		add(new GamePanel(new CardLayout()));
 		
 		setVisible(true);
 
-	}
-
-	public void addStart() {
-
-		final int BTN_WIDTH = 300;
-		final int BTN_HEIGHT = 100;
-		final int BTN_X = WIDTH / 2 - BTN_WIDTH / 2;
-		final int BTN_Y = HEIGHT - BTN_HEIGHT * 3;
-
-		startPanel = new StartPagePanel();
-		roundPanel = new RoundPanel();
-		
-		startPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		startPanel.setLayout(null);
-		roundPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		roundPanel.setLayout(null);
-
-		btn = new JButton("BEGIN");
-		btn.setBounds(BTN_X, BTN_Y, BTN_WIDTH, BTN_HEIGHT);
-		btn.setFocusPainted(false);
-		btn.setFont(new Font("Arial", Font.PLAIN, 40));
-		btn.setBackground(new Color(98, 189, 175));
-		btn.setForeground(Color.white);
-		
-		roundPanel.setVisible(false);
-		startPanel.setVisible(true);
-		
-		
-		startPanel.add(btn);
-		
-		add(roundPanel);
-		add(startPanel);
-		
-	}
-
-	public void buttonClicked() {
-		btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				startPanel.setVisible(false);
-				roundPanel.setVisible(true);
-			}
-		});
 	}
 }
