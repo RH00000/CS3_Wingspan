e126056dc0cb43207e970719d2ab9bddef699d06
WangCatherineK1223048 <k1223048@students.katyisd.org>
2025-11-06 09:13:01 -0600

    Refactor GameStatePanel to extend JPanel and initialize fields

--- GameStatePanel.java
+++ GameStatePanel.java
@@ -11,23 +11,39 @@ import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import java.util.*;
 
-public class GameStatePanel {
-	public ArrayList<Integer> numFood = new ArrayList<>();
-	public ArrayList<Integer> numCards = new ArrayList<>();
+public class GameStatePanel extends JPanel {
+	
+	public ArrayList<Integer> numFood;
+	public ArrayList<Integer> numCards;
+	public ArrayList<Integer> foodInside;
 	public int numActions;
 	public int numEggs;
 	public boolean isFirst;
 	
-	public void paint(Graphics g) {
+	public GameStatePanel() {
+		
+		numFood = new ArrayList<>();
+		numCards = new ArrayList<>();
+		foodInside = new ArrayList<>();
+		numActions = 8;
+		numEggs = 0;
+		isFirst = true;
 		
 	}
 	
-	public void drawGameState(Graphics g) {
+	public void drawGameState (Graphics g) {
+		
+		super.paint(g);
 		
 	}
 	
 	public void updateGameState(ArrayList<Integer> numFood, ArrayList<Integer> foodInside, ArrayList<Integer> numCards, int numActions, int numEggs, boolean isFirst ) {
 		
-		
+		this.numFood = numFood;
+		this.foodInside = foodInside;
+		this.numCards = numCards;
+		this.numActions = numActions;
+		this.numEggs = numEggs;
+		this.isFirst = isFirst;
 	}
 }
