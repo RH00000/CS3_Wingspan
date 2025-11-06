d2fed3c9b28a1ae68e5e0b1c710deb787a8d86bf
NiyateeD <K1638427@students.katyisd.org>
2025-11-06 09:10:15 -0600

    Update ActionPanel.java

--- ActionPanel.java
+++ ActionPanel.java
@@ -7,6 +7,7 @@ import java.io.*;
 import java.awt.image.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+import java.awt.Font;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
@@ -17,49 +18,69 @@ public class ActionPanel extends JPanel{
 	public JButton layEggsBtn = new JButton("Lay Eggs");
 	public JButton drawCardsBtn = new JButton("Draw Bird");
 	public JButton playBirdBtn = new JButton("Play Bird");
-	public JButton exchangeFood = new JButton("Exchange");
-	public JButton exchangeDice = new JButton();
-	public JButton exchangeCard = new JButton();
+	public JButton exchangeFood = new JButton("Exchange Food");
+	public JButton exchangeDice = new JButton("Exchange Dice");
+	public JButton exchangeCard = new JButton("Exchange Card");
 	public JButton rerollDice = new JButton("ReRoll Dice");
+	public JButton seeOtherBoard = new JButton("View Other Board");
 
 	public ActionPanel() {
-		
-		setPreferredSize(new Dimension(1600, 100));
-		//setBackground(Color.red); //testing only
-		
-		try {			
-			setLayout(new FlowLayout());
-			this.add(gainFoodBtn);
-			this.add(layEggsBtn);
-			this.add(drawCardsBtn);
-			this.add(playBirdBtn);
-			this.add(exchangeFood);
-			this.add(rerollDice);
-
+		try {
 			setLayout(null);
 			gainFoodBtn.setBounds(10, 10, 100, 30);
-			gainFoodBtn.setBackground(new Color(98, 189, 175));
+			gainFoodBtn.setBackground(Color.blue);
 			gainFoodBtn.setForeground(Color.white);
+			this.add(gainFoodBtn);
 			
+			setLayout(null);
 			layEggsBtn.setBounds(120, 10, 100, 30);
-			layEggsBtn.setBackground(new Color(98, 189, 175));
+			layEggsBtn.setBackground(Color.blue);
 			layEggsBtn.setForeground(Color.white);
+			this.add(layEggsBtn);
 			
+			setLayout(null);
 			drawCardsBtn.setBounds(230, 10, 100, 30);
-			drawCardsBtn.setBackground(new Color(98, 189, 175));
+			drawCardsBtn.setBackground(Color.blue);
 			drawCardsBtn.setForeground(Color.white);
+			this.add(drawCardsBtn);
 			
+			setLayout(null);
 			playBirdBtn.setBounds(340, 10, 100, 30);
-			playBirdBtn.setBackground(new Color(98, 189, 175));
+			playBirdBtn.setBackground(Color.blue);
 			playBirdBtn.setForeground(Color.white);
+			this.add(playBirdBtn);
 			
+			setLayout(null);
 			exchangeFood.setBounds(340, 10, 100, 30);
-			exchangeFood.setBackground(new Color(98, 189, 175));
+			exchangeFood.setBackground(Color.blue);
 			exchangeFood.setForeground(Color.white);
+			this.add(exchangeFood);
 			
+			setLayout(null);
 			rerollDice.setBounds(450, 10, 100, 30);
-			rerollDice.setBackground(new Color(98, 189, 175));
+			rerollDice.setBackground(Color.blue);
 			rerollDice.setForeground(Color.white);
+			this.add(rerollDice);
+			
+			setLayout(null);
+			exchangeFood.setBounds(560, 10, 120, 30);
+			exchangeFood.setBackground(Color.blue);
+			exchangeFood.setForeground(Color.white);
+			this.add(exchangeFood);
+			
+			setLayout(null);
+			exchangeCard.setBounds(690, 10, 120, 30);
+			exchangeCard.setBackground(Color.blue);
+			exchangeCard.setForeground(Color.white);
+			this.add(exchangeCard);
+			
+			
+			setLayout(null);
+			seeOtherBoard.setBounds(690+140, 10, 150, 30);
+			seeOtherBoard.setBackground(Color.blue);
+			seeOtherBoard.setForeground(Color.white);
+			this.add(seeOtherBoard);
+			
 		}
 		
 		catch(Exception e) {
