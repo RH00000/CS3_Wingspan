1af62bd77e5111d51777f9b68b32e31c8e4ade8b
NiyateeD <K1638427@students.katyisd.org>
2025-11-10 08:46:50 -0600

    Create EndPanel.java

---
+++ EndPanel.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class EndPanel {
+
+	//private HashMap<Integer, ArrayList<Bird>> board1;
+	//private HashMap<Integer, ArrayList<Bird>> board2;
+	//private ArrayList<Bonus> bonusCards1
+	//private ArrayList<Bonus> bonusCards2
+	//private HashMap<RoundGoal, int[]> goalBoard;
+	//private HashMap<Player, int[]> scoreSummary;
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void drawEndScreen(Graphics g) {
+		paint(g);
+	}
+}
